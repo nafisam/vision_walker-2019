7d03c3e4e7f7d09bad0df50832026644c1bbcd90
kjfullin <kfullin@ashland.edu>
2019-06-24 18:02:22 -0400

    Update obstacle_detection.py
    Added notes, questions, and removed unnecessary variables

--- DepthImages/obstacle_detection.py
+++ DepthImages/obstacle_detection.py
@@ -8,7 +8,7 @@ def detect(input, width, threshold):
     array = input
 
     # Only required if we're not averageing the array by this point.
-    # Gets the 640 x 480 array and turns it into a 1-d array across the middle.
+    # Gets the 640 x 480 array and turns it into a 1-d array, using the middle value instead of the average
     if len(np.shape(array)) > 1:
         h, w = np.shape(array)
         array = array[:, w / 2]
@@ -17,6 +17,7 @@ def detect(input, width, threshold):
     array = array[array != 0]
 
     # For now we assume super close object mostly filling screen = bad.
+    # The following 2 lines return 1 if there are too many error states in the array, 1 means nothing happens in test_haptics
     if len(array) < 200:
         return 1
 
@@ -31,26 +32,27 @@ def detect(input, width, threshold):
 
     while index < len(array) - width:
         # Slope calculation
-        x1 = index
         y1 = array[index]
-        x2 = index + width
         y2 = array[index + width]
 
-        slope = (y2 - y1) / (x2 - x1)
+        slope = (y2 - y1) / width
 
-        index += width
+        index += width	# why are we doing this? it skips <width> points, is it just to be faster? operating under the assumption that any points between those we're comparing would yield similar results
 
         # Break out of the loop if we have a non-positive slope.
-        if slope < -2:
+	# This is the case of a regular obstacle
+        if slope < -2:	# why -2? just to give a little leeway for error?
             break
         
-        if slope > 200:
-            return y1
+	# This is the case of a drop-off, ie stairs, curb, etc.
+        if slope > 200:	# any reason why 200?
+            return y1	# by returning y1 we will trigger the obstacle detection, however I think the distance away may be not be a good representation
+			# y1 is the depth of the obstacle so if it was a very steep drop it might return a far away distance even if the drop point was close, I think returning y2 might fix this?
 
     # Continue where we left off from ^
     # Find the lowest y-value, corresponding to the closest point.
-    while index < len(array):
-        if array[index] < distance:
+    while index < len(array):			# how does this loop prevent it reporting the closest point if the closest point is empty ground in front of it?
+        if array[index] < distance:		# could we fix this by putting just these middle 2 lines inside the "if slope < -2:" instead of break?
             distance = array[index]
         index += 1
     if DEBUG:
@@ -83,12 +85,10 @@ def detect_file(input_file, width, threshold):
     distance = 9999
 
     while index < len(array) - width:
-        x1 = index
         y1 = array[index]
-        x2 = index + width
         y2 = array[index + width]
 
-        slope = (y2 - y1) / (x2 - x1)
+        slope = (y2 - y1) / width
 
         index += width
 
